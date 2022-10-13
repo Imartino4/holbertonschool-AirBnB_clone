ad473bd3a0047367af60f9681ebc7c5753500cb7
blas <blashernandez98@gmail.com>
2022-10-13 16:02:39 -0300

    Testing filestorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Unittest """
+
+import unittest
+from models.base_model import BaseModel
+from models import storage
+import os
+
+
+class TestFileStorage(unittest.TestCase):
+    """ Test FileStorage class """
+
+    def setUp(self):
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+        storage.reload()
+        self.obj1 = BaseModel()
+        self.obj2 = BaseModel()
+        storage.save()
+
+    def test_all(self):
+        amount_objs = len(storage.all())
+        self.assertEqual(amount_objs, 2)
+
+    def test_new(self):
+        obj = BaseModel()
+        storage.new(obj)
+        self.assertTrue(obj in storage.all().values())
