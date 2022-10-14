39e11adbcabd66496f6caf8c9c284a8ed67566e1
blas <blashernandez98@gmail.com>
2022-10-14 10:47:27 -0300

    User tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,15 +8,7 @@ from models.user import User
 class TestUser(unittest.TestCase):
     """ Tests on BaseModel class"""
 
-    def test_init(self):
-        """Test when initialize a new object"""
-        obj1 = User()
-        self.assertEqual(type(obj1.id), str)
-
     def test_user_email(self):
         """Check attributes type """
         obj2 = User()
         self.assertTrue(hasattr(obj2, 'email'))
-        self.assertTrue(hasattr(obj2, 'password'))
-        self.assertTrue(hasattr(obj2, 'first_name'))
-        self.assertTrue(hasattr(obj2, 'last_name'))
