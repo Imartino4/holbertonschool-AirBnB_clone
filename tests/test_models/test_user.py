e7314f38a800326b0ff029ef8b55e34977aa266d
blas <blashernandez98@gmail.com>
2022-10-14 10:58:18 -0300

    Trying to understand the checker

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,3 +11,18 @@ class TestUser(unittest.TestCase):
         """ Check attributes type """
         user = User()
         self.assertTrue(hasattr(user.__class__, 'email'))
+
+    def test_user_password(self):
+        """ Check attributes type """
+        user = User()
+        self.assertTrue(hasattr(user.__class__, 'password'))
+
+    def test_user_first_name(self):
+        """ Check attributes type """
+        user = User()
+        self.assertTrue(hasattr(user.__class__, 'first_name'))
+
+    def test_user_last_name(self):
+        """ Check attributes type """
+        user = User()
+        self.assertTrue(hasattr(user.__class__, 'last_name'))
