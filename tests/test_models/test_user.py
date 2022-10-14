141847f8d661e24ea7f479bcc78d89dd81b704c0
blas <blashernandez98@gmail.com>
2022-10-14 20:54:13 -0300

    Added tests for classes: User, Review and Amenity

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -7,22 +7,7 @@ from models.user import User
 class TestUser(unittest.TestCase):
     """ Tests on BaseModel class"""
 
-    def test_user_email(self):
-        """ Check attributes type """
-        user = User()
-        self.assertEqual(type(user.email), str)
-
-    def test_user_first_name(self):
-        """ Check attributes type """
-        user = User()
-        self.assertEqual(type(user.first_name), str)
-
-    def test_user_last_name(self):
-        """ Check attributes type """
-        user = User()
-        self.assertEqual(type(user.last_name), str)
-
-    def test_user_password(self):
+    def test_user(self):
         """ Check attribute type """
         user = User()
         self.assertEqual(type(user.password), str)
