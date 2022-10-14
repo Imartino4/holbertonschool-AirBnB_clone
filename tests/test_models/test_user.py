7915ec650c2e25c4efb9d4dd34bb5a886f294712
blas <blashernandez98@gmail.com>
2022-10-14 10:54:41 -0300

    User tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,13 +2,12 @@
 """ Unittest """
 
 import unittest
-from models.base_model import BaseModel
 from models.user import User
 
 class TestUser(unittest.TestCase):
     """ Tests on BaseModel class"""
 
     def test_user_email(self):
-        """Check attributes type """
-        obj2 = User()
-        self.assertTrue(hasattr(obj2, 'email'))
+        """ Check attributes type """
+        user = User()
+        self.assertTrue(hasattr(user.__class__, 'email'))
