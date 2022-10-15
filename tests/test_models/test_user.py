381a18529cbcb74464775608feac5769f385acd8
blas <blashernandez98@gmail.com>
2022-10-14 21:03:00 -0300

    User Tests Fix

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -7,7 +7,23 @@ from models.user import User
 class TestUser(unittest.TestCase):
     """ Tests on BaseModel class"""
 
-    def test_user(self):
+    def test_user_password(self):
         """ Check attribute type """
         user = User()
         self.assertEqual(type(user.password), str)
+
+    def test_user_email(self):
+        """ Check attribute type """
+        user = User()
+        self.assertEqual(type(user.email), str)
+
+    
+    def test_user_last_name(self):
+        """ Check attribute type """
+        user = User()
+        self.assertEqual(type(user.last_name), str)
+
+    def test_user_first_name(self):
+        """ Check attribute type """
+        user = User()
+        self.assertEqual(type(user.first_name), str)
